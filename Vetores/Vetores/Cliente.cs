983fa6754b95a5c4f3b9145b1d9e393f1338cf69
unknown <thiago.ferreira164@etec.sp.gov.br>
2024-05-15 17:50:39 -0300

    atualizado

---
+++ Vetores/Vetores/Cliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    public class Cliente
+    {
+        protected string nome;
+        protected string dataDeNascimento;
+        protected string email;
+        protected string telefone;
+        protected string endereço;
+        int i;
+        int[] lista = new int[9];
+
+        public void cadastrar()
+        {
+            Console.WriteLine("Digite o nome do cliente:");
+            this.nome = Console.ReadLine();
+            Console.WriteLine("Digite a data de nascimento:");
+            this.dataDeNascimento = Console.ReadLine();
+            Console.WriteLine("Digite o email:");
+            this.email = Console.ReadLine();
+            Console.WriteLine("Digite o telefone:");
+            this.telefone = (Console.ReadLine());
+            Console.WriteLine("Digite o endereço:");
+            this.endereço = Console.ReadLine();
+        }
+
+        public void mostrarDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Data de nascimento: " + dataDeNascimento);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Telefone: " + telefone);
+            Console.WriteLine("Endereço: " + endereço);
+        }
+
+        public void fila()
+        {
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Nome:" + nome);
+            }
+        }
+
+        public void 
+    }
+
+
+}
---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    public class Program
+    {
+        // protected int tamanhoDaFila = 9;
+        
+        static void Main(string[] args)
+        {
+
+
+            
+            Console.WriteLine("Cadastro do cliente");
+
+            Cliente novoCliente = new Cliente();
+            novoCliente.cadastrar();
+            Console.WriteLine("Confirme os dados abaixo:");
+            
+            novoCliente.mostrarDados();
+
+
+            Console.WriteLine("Fila de espera:");
+            novoCliente.fila();
+          
+               
+
+
+            //int[] vetor = new int[9];
+            //for (int i = 0; i < 9; i++)
+            //{
+            //  vetor[i] = novoCliente.
+            //}
+
+        }
+
+        //public void filaClientes()
+        //{
+          //  Console.WriteLine("Clientes:");
+            //for (int i = 0; i < tamanhoDaFila; i++)
+           // {
+              //  Console.WriteLine(i);
+
+            //}
+
+        }
+
+    }
