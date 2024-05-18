de4b3d084c7d111ff66bce7816a834f9845641af
nicole <nicole.goncalves6@etec.sp.gov.br>
2024-05-18 15:40:56 -0300

    att

--- Vetores/Program.cs
+++ Vetores/Program.cs
@@ -11,11 +11,52 @@ namespace Vetores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cadastro do Cliente");
-            cliente um = new cliente();
-            um.cadastrar();
-            Console.WriteLine("Confirme os dados abaixo:");
-            um.mostrarDados();
+           
+            Cliente novoCliente = new Cliente();
+
+            string opcao;
+
+            while (true)
+            {
+                Console.WriteLine(
+                    "1- Cadastro do cliente\n " +
+                    "2- Prioridade\n " +
+                    "3- Consultar fila\n " +
+                    "4- Atender cliente\n q- Sair");
+
+
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        novoCliente.Cadastrar();
+                        novoCliente.Fila();
+                        break;
+
+                    case "2":
+                        novoCliente.Cadastrar();
+                        novoCliente.FilaPrioridade();
+                        break;
+
+                    case "3":
+                        novoCliente.ConsultarFila();
+                        break;
+
+                    case "4":
+                        novoCliente.Atender();
+                        break;
+
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        break;
+                     
+                }
+            }
         }
 
 
--- Vetores/cliente.cs
+++ Vetores/cliente.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Vetores
 {
-    public class cliente
+    public class Cliente
     {
+        int f;
+        int i;
+        protected int[] vetor;
+        protected int fim;
+        protected int capacidade;
+        protected int tamanho;
+        protected int fila;
         protected int id;
         protected string nome;
         protected string dataDeNascimento;
@@ -15,29 +22,73 @@ namespace Vetores
         protected string telefone;
         protected string endereço;
 
-        public void cadastrar()
+        public void Cadastrar()
         {
-            Console.WriteLine("Digite o nome do cliente:");
-            this.nome = Console.ReadLine();
-            Console.WriteLine("Digite a data de nascimento:");
-            this.dataDeNascimento = Console.ReadLine();
-            Console.WriteLine("Digite o email:");
-            this.email = Console.ReadLine();
-            Console.WriteLine("Digite o telefone:");
-            this.telefone = (Console.ReadLine());
-            Console.WriteLine("Digite o endereço:");
-            this.endereço = Console.ReadLine();
+            if (fila < 10)
+            {
+                Console.WriteLine("Digite o nome do cliente:");
+                this.nome = Console.ReadLine();
+                Console.WriteLine("Digite a data de nascimento:");
+                this.dataDeNascimento = Console.ReadLine();
+                Console.WriteLine("Digite o email:");
+                this.email = Console.ReadLine();
+                Console.WriteLine("Digite o telefone:");
+                this.telefone = (Console.ReadLine());
+                Console.WriteLine("Digite o endereço:");
+                this.endereço = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("A fila está cheia");
+            }
         }
 
-        public void mostrarDados()
+        public void MostrarDados()
         {
-            Console.WriteLine("Nome: " +nome);
-            Console.WriteLine("Data de nascimento: " +dataDeNascimento);
-            Console.WriteLine("Email: " +email);
-            Console.WriteLine("Telefone: " +telefone);
-            Console.WriteLine("Endereço: " +endereço);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Data de nascimento: " + dataDeNascimento);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Telefone: " + telefone);
+            Console.WriteLine("Endereço: " + endereço);
         }
+
+        public void Fila()
+        {
+           
+        }
+
+        public void FilaPrioridade()
+        {
+
+        }
+
+        public void ConsultarFila()
+        {
+            if (tamanho == 0)
+            {
+                Console.WriteLine("Fila vazia.");
+            }
+            else
+            {
+                Console.WriteLine("Fila:");
+
+                while (fim == f)
+                {
+                    Console.WriteLine(vetor[i]);
+                    i = (i + 1) % capacidade;
+                }
+                Console.WriteLine(vetor[f]);
+            }
+        }
+
+        public void Atender()
+        {
+
+            Console.WriteLine();
+        }
+
+     
     }
 
-
 }
+
