4f4222602f7332c33967caf11791ec38a1b9c975
eduarda <eduarda.santos202@etec.sp.gov.br>
2024-05-18 21:26:08 -0300

    att

--- Vetores/Program.cs
+++ Vetores/Program.cs
@@ -11,93 +11,75 @@ namespace Vetores
     {
         static void Main(string[] args)
         {
-            
+        
             Cliente novoCliente = new Cliente();
 
-            string opcao;
+             string opcao;
 
-            while (true)
-            {
-                Console.WriteLine("\nEscola uma opção abaixo:");
-                Console.WriteLine(
-                    "\n 1- Cadastro do cliente\n " +
-                    "2- Prioridade\n " +
-                    "3- Consultar fila\n " +
-                    "4- Atender cliente\n q- Sair\n");
+             while (true)
+             {
+                 Console.WriteLine("\nEscola uma opção abaixo:");
+                 Console.WriteLine(
+                     "\n 1- Cadastro do cliente\n " +
+                     "2- Prioridade\n " +
+                     "3- Consultar fila\n " +
+                     "4- Atender cliente\n " +
+                     "q- Sair\n");
 
 
-                opcao = Console.ReadLine();
+                 opcao = Console.ReadLine();
 
-                switch (opcao)
-                {
-                    case "1":
-                        novoCliente.Cadastrar();
-                        Console.WriteLine("\nConfirme os dados abaixo:\n");
-                        novoCliente.MostrarDados();
-                        Console.WriteLine("\n Os dados estão corretos S/N\n");
-                        string resposta = Console.ReadLine();
+                 switch (opcao)
+                 {
+                     case "1":
+                         novoCliente.Cadastrar();
+                         Console.WriteLine("\nConfirme os dados abaixo:\n");
+                         novoCliente.MostrarDados();
+                         Console.WriteLine("\n Os dados estão corretos S/N\n");
+                         string resposta = Console.ReadLine();
 
-                        if (resposta.ToUpper() == "S")
-                        {
-                            novoCliente.Fila();
-                            Console.WriteLine("\nCadastro feito com sucesso!!\n Cliente adicionado a fila de espera\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\nFaça o cadastro novamente\n");
-                            novoCliente.Cadastrar();
-                            novoCliente.MostrarDados();
-                            Console.WriteLine("\n Os dados estão corretos S/N\n");
-                            Console.ReadLine();
-                            novoCliente.Fila();
-                            Console.WriteLine("\nCadastro feito com sucesso!!\n Cliente adicionado a fila de espera\n");
-                        }
+                         if (resposta.ToUpper() == "S")
+                         {
+                             Console.WriteLine("\nCadastro feito com sucesso!!\n Cliente adicionado a fila de espera\n");
+                         }
+                         else
+                         {
+                             Console.WriteLine("\nFaça o cadastro novamente\n");
+                             novoCliente.Cadastrar();
+                             novoCliente.MostrarDados();
+                             Console.WriteLine("\n Os dados estão corretos S/N\n");
+                             Console.ReadLine();
+                             Console.WriteLine("\nCadastro feito com sucesso!!\n Cliente adicionado a fila de espera\n");
+                         }
+                         break;
+
+                     case "2":
+
+                        novoCliente.FilaPrioridade();
                         break;
 
-                    case "2":
-                        novoCliente.Cadastrar();
-                        Console.WriteLine("\nConfirme os dados abaixo:\n");
-                        novoCliente.MostrarDados();
-                        Console.WriteLine("\n Os dados estão corretos S/N\n");
-                        resposta = Console.ReadLine();
+                     case "3":
 
-                        if (resposta.ToUpper() == "S")
-                        {
-                            novoCliente.FilaPrioridade();
-                            Console.WriteLine("\nCadastro feito com sucesso!!\n Cliente adicionado a fila de espera\n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\nFaça o cadastro novamente\n");
-                            novoCliente.Cadastrar();
-                            Console.WriteLine("\n Os dados estão corretos S/N\n");
-                            novoCliente.MostrarDados();
-                            Console.ReadLine();
-                            novoCliente.FilaPrioridade();
-                            Console.WriteLine("\nCadastro feito com sucesso!!\n Cliente adicionado a fila de espera\n");
-                        }
-                        break;
+                         novoCliente.ConsultarFila();
+                         break;
 
-                    case "3":
-                        novoCliente.ConsultarFila();
-                        break;
+                     case "4":
+                         novoCliente.Atender();
+                         break;
 
-                    case "4":
-                        novoCliente.Atender();
-                        break;
+                     case "q":
+                         Environment.Exit(0);
+                         break;
 
-                    case "q":
-                        Environment.Exit(0);
-                        break;
+                     default:
+                         Console.WriteLine("\nOpção inválida! Tente novamente.");
+                         break;
 
-                    default:
-                        Console.WriteLine("\nOpção inválida! Tente novamente.");
-                        break;
-                     
-                }
-            }
+                 }
+             }
         }
 
 
     }
 }
+
--- Vetores/cliente.cs
+++ Vetores/cliente.cs
@@ -8,87 +8,135 @@ namespace Vetores
 {
     public class Cliente
     {
-        int f;
-        int i;
-        protected int[] vetor;
-        protected int fim;
-        protected int capacidade;
-        protected int tamanho;
-        protected int fila;
-        protected int id;
-        protected string nome;
-        protected string dataDeNascimento;
-        protected string email;
-        protected string telefone;
-        protected string endereço;
+       
+            Cliente pessoa;
+            protected Cliente[] vetor;
+            protected int capacidade = 10;
+            protected int inicio = 0;
+            protected int prioridade = 0;
+            protected int fila = 0;
+            public Cliente cli_prioridade;
 
-        public void Cadastrar()
-        {
-            if (fila < 10)
+            protected string nome;
+            protected string dataDeNascimento;
+            protected string email;
+            protected string telefone;
+            protected string endereço;
+
+            public Cliente()
             {
-                Console.WriteLine("Digite o nome do cliente:");
-                this.nome = Console.ReadLine();
-                Console.WriteLine("Digite a data de nascimento:");
-                this.dataDeNascimento = Console.ReadLine();
-                Console.WriteLine("Digite o email:");
-                this.email = Console.ReadLine();
-                Console.WriteLine("Digite o telefone:");
-                this.telefone = (Console.ReadLine());
-                Console.WriteLine("Digite o endereço:");
-                this.endereço = Console.ReadLine();
+                vetor = new Cliente[capacidade];
             }
-            else
+
+            public void Cadastrar()
             {
-                Console.WriteLine("A fila está cheia");
-            }
-        }
-
-        public void MostrarDados()
-        {
-            Console.WriteLine("Nome: " + nome);
-            Console.WriteLine("Data de nascimento: " + dataDeNascimento);
-            Console.WriteLine("Email: " + email);
-            Console.WriteLine("Telefone: " + telefone);
-            Console.WriteLine("Endereço: " + endereço);
-        }
-
-        public void Fila()
-        {
-           
-        }
-
-        public void FilaPrioridade()
-        {
-
-        }
-
-        public void ConsultarFila()
-        {
-            if (tamanho == 0)
-            {
-                Console.WriteLine("Fila vazia.");
-            }
-            else
-            {
-                Console.WriteLine("Fila:");
-
-                while (fim == f)
+                if (fila < capacidade)
                 {
-                    Console.WriteLine(vetor[i]);
-                    i = (i + 1) % capacidade;
+                    pessoa = new Cliente();
+                    Console.WriteLine("Digite o nome do cliente:");
+                    pessoa.nome = Console.ReadLine();
+                    Console.WriteLine("Digite a data de nascimento:");
+                    pessoa.dataDeNascimento = Console.ReadLine();
+                    Console.WriteLine("Digite o email:");
+                    pessoa.email = Console.ReadLine();
+                    Console.WriteLine("Digite o telefone:");
+                    pessoa.telefone = Console.ReadLine();
+                    Console.WriteLine("Digite o endereço:");
+                    pessoa.endereço = Console.ReadLine();
+                    vetor[inicio] = pessoa;
+                    inicio++;
+                    fila++;
                 }
-                Console.WriteLine(vetor[f]);
+                else
+                {
+                    Console.WriteLine("A fila está cheia");
+                }
+            }
+
+            public void MostrarDados()
+            {
+                Console.WriteLine("Nome: " + this.nome);
+                Console.WriteLine("Data de nascimento: " + this.dataDeNascimento);
+                Console.WriteLine("Email: " + this.email);
+                Console.WriteLine("Telefone: " + this.telefone);
+                Console.WriteLine("Endereço: " + this.endereço);
+            }
+
+            public void FilaPrioridade()
+            {
+                if (fila < capacidade)
+                {
+                    cli_prioridade = new Cliente();
+                    Console.WriteLine("Digite o nome do cliente prioritário:");
+                    cli_prioridade.nome = Console.ReadLine();
+                    Console.WriteLine("Digite a data de nascimento:");
+                    cli_prioridade.dataDeNascimento = Console.ReadLine();
+                    Console.WriteLine("Digite o email:");
+                    cli_prioridade.email = Console.ReadLine();
+                    Console.WriteLine("Digite o telefone:");
+                    cli_prioridade.telefone = Console.ReadLine();
+                    Console.WriteLine("Digite o endereço:");
+                    cli_prioridade.endereço = Console.ReadLine();
+
+                    for (int s = fila - 1; s >= prioridade; s--)
+                    {
+                        vetor[s + 1] = vetor[s];
+                    }
+
+                    vetor[prioridade] = cli_prioridade;
+                    fila++;
+                    prioridade++;
+                }
+                else
+                {
+                    Console.WriteLine("A fila está cheia");
+                }
+            }
+
+            public void ConsultarFila()
+            {
+                Console.WriteLine("Clientes na fila:");
+
+                for (int i = 0; i < prioridade; i++)
+                {
+                    Console.WriteLine("Cliente Prioritário " + (i + 1));
+                    if (vetor[i] != null)
+                    {
+                        vetor[i].MostrarDados();
+                    }
+                    Console.WriteLine();
+                }
+
+                for (int i = prioridade; i < fila; i++)
+                {
+                    Console.WriteLine("Cliente " + (i + 1));
+                    if (vetor[i] != null)
+                    {
+                        vetor[i].MostrarDados();
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+            public void Atender()
+            {
+            if (fila > 0)
+            {
+                Cliente clienteAtendido = vetor[0];
+                for (int i = 0; i < fila - 1; i++)
+                {
+                    vetor[i] = vetor[i + 1];
+                }
+                fila--;
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("o");
             }
         }
-
-        public void Atender()
-        {
-
-            Console.WriteLine();
-        }
-
-     
     }
-
 }
+
 
